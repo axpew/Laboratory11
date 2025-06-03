f57d5bbaead9c6e4e30556feed41ba5c60a48b75
Gilberth Chaves A <gchavesav@gmail.com>
2025-06-03 15:08:48 -0600

    Initial commit Lab 11 Profesor

---
+++ src/main/java/domain/AdjacencyMatrixGraph.java
@@ -0,0 +1,169 @@
+package domain;
+
+import domain.list.ListException;
+import domain.queue.LinkedQueue;
+import domain.queue.QueueException;
+import domain.stack.LinkedStack;
+import domain.stack.StackException;
+
+public class AdjacencyMatrixGraph implements Graph {
+    private Vertex[] vertexList; //arreglo de objetos tupo vértice
+    private Object[][] adjacencyMatrix; //arreglo bidimensional
+    private int n; //max de elementos
+    private int counter; //contador de vertices
+
+    //para los recorridos dfs, bfs
+    private LinkedStack stack;
+    private LinkedQueue queue;
+
+    //Constructor
+    public AdjacencyMatrixGraph(int n) {
+        if (n <= 0) System.exit(1); //sale con status==1 (error)
+        this.n = n;
+        this.counter = 0;
+        this.vertexList = new Vertex[n];
+        this.adjacencyMatrix = new Object[n][n];
+        this.stack = new LinkedStack();
+        this.queue = new LinkedQueue();
+        initMatrix(); //inicializa matriz de objetos con cero
+    }
+
+    private void initMatrix() {
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                this.adjacencyMatrix[i][j] = 0; //init con ceros
+    }
+
+    @Override
+    public int size() throws ListException {
+        return counter;
+    }
+
+    @Override
+    public void clear() {
+        this.vertexList = new Vertex[n];
+        this.adjacencyMatrix = new Object[n][n];
+        this.counter = 0; //inicializo contador de vértices
+        this.initMatrix();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return counter == 0;
+    }
+
+    @Override
+    public boolean containsVertex(Object element) throws GraphException, ListException {
+        return false;
+    }
+
+    @Override
+    public boolean containsEdge(Object a, Object b) throws GraphException, ListException {
+        return false;
+    }
+
+    @Override
+    public void addVertex(Object element) throws GraphException, ListException {
+
+    }
+
+    @Override
+    public void addEdge(Object a, Object b) throws GraphException, ListException {
+
+    }
+
+    @Override
+    public void addWeight(Object a, Object b, Object weight) throws GraphException, ListException {
+
+    }
+
+    @Override
+    public void addEdgeWeight(Object a, Object b, Object weight) throws GraphException, ListException {
+
+    }
+
+    @Override
+    public void removeVertex(Object element) throws GraphException, ListException {
+
+    }
+
+    @Override
+    public void removeEdge(Object a, Object b) throws GraphException, ListException {
+
+    }
+
+    // Recorrido en profundidad
+    @Override
+    public String dfs() throws GraphException, StackException, ListException {
+        setVisited(false);//marca todos los vertices como no vistados
+        // inicia en el vertice 0
+        String info = vertexList[0].data + ", ";
+        vertexList[0].setVisited(true); // lo marca
+        stack.clear();
+        stack.push(0); //lo apila
+        while (!stack.isEmpty()) {
+            // obtiene un vertice adyacente no visitado,
+            //el que esta en el tope de la pila
+            int index = adjacentVertexNotVisited((int) stack.top());
+            if (index == -1) // no lo encontro
+                stack.pop();
+            else {
+                vertexList[index].setVisited(true); // lo marca
+                info += vertexList[index].data + ", "; //lo muestra
+                stack.push(index); //inserta la posicion
+            }
+        }
+        return info;
+    }
+
+    //Recorrido en amplitud
+    @Override
+    public String bfs() throws GraphException, QueueException, ListException {
+        setVisited(false);//marca todos los vertices como no visitados
+        // inicia en el vertice 0
+        String info = vertexList[0].data + ", ";
+        vertexList[0].setVisited(true); // lo marca
+        queue.clear();
+        queue.enQueue(0); // encola el elemento
+        int v2;
+        while (!queue.isEmpty()) {
+            int v1 = (int) queue.deQueue(); // remueve el vertice de la cola
+            // hasta que no tenga vecinos sin visitar
+            while ((v2 = adjacentVertexNotVisited(v1)) != -1) {
+                // obtiene uno
+                vertexList[v2].setVisited(true); // lo marca
+                info += vertexList[v2].data + ", "; //lo muestra
+                queue.enQueue(v2); // lo encola
+            }
+        }
+        return info;
+    }
+
+    //setteamos el atributo visitado del vertice respectivo
+    private void setVisited(boolean value) {
+        for (int i = 0; i < counter; i++) {
+            vertexList[i].setVisited(value); //value==true o false
+        }//for
+    }
+
+    private int adjacentVertexNotVisited(int index) {
+        for (int i = 0; i < counter; i++) {
+            if (!adjacencyMatrix[index][i].equals(0)
+                    && !vertexList[i].isVisited())
+                return i;//retorna la posicion del vertice adyacente no visitado
+        }//for i
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Adjacency Matrix Graph Content...";
+        //se muestran todos los vértices del grafo
+        for (int i = 0; i < counter; i++) {
+            result+="\nThe vextex in the position: "+i+" is: "+vertexList[i].data;
+        }
+
+        return result;
+    }
+}
